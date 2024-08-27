e0a5c87ec7e491423c638a56cbea3e92e57ac875
Nikunj Rathod <nikunjrathod3011@gmail.com>
2024-08-27 11:14:47 -0300

    updates

--- app/src/main/java/space/models/api/character/AgentResponseItem.kt
+++ app/src/main/java/space/models/api/character/AgentResponseItem.kt
@@ -3,12 +3,6 @@ package space.models.api.character
 import space.models.api.characterdetail.CardImage
 
 data class AgentResponseItem(
-    val element: String,
-    val rarity: String,
-    val faction: String,
-    val style: String,
     val name: String,
-    val cardImage: CardImage,
     val image: String,
-    val slug: String,
 )
